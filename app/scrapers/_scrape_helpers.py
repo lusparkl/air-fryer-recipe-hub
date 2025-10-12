814ad6936bf00cea35ce45f678d0e042fa38e9c1
lusparkl <flypyfl@gmail.com>
2025-10-12 12:19:53 +0300

    Refactor scraper helpers and move get_suitable_links
    Moved the get_suitable_links function and related constants and imports from _utils.py to a new _scrape_helpers.py module. This improves code organization by separating scraping helpers from utility functions.

--- app/scrapers/_scrape_helpers.py
+++ app/scrapers/_scrape_helpers.py
@@ -1,30 +1,11 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-}
-
-def nutrion_facts_to_int(fact: str):
-    return int(fact.replace("g", ""))
-
-def pretiffy_strings_list(strings_list: list):
-    list_pretiffied = [i.text.replace("\n", "").strip() for i in strings_list]
-    return list_pretiffied
-
-def time_details_in_minutes(raw_time: list):
-    result = []
-    for time in raw_time:
-            time_list_raw = time.split("hr")
-            time_list = [int(t.replace("mins", "")) for t in time_list_raw]
-            if len(time_list) == 2:
-                result.append(time_list[0] * 60 + time_list[1])
-            else:
-                result.append(time_list[0])
-    
-    return result
-
+from allrecipes import AllRecipes
+from eatingwell import EatingWell
+from seriouseats import SeriousEats
+from simplyrecipes import SimplyRecipies
+from spruceeats import SpruceEats
 
 BASE_LINK = "https://www.myrecipes.com/search?q=air+fryer&offset="
 SUITABLE_WEBSITES = ["Allrecipes", "The Spruce Eats", "EatingWell", "Simply Recipes", "Serious Eats"]
@@ -53,4 +34,21 @@ def get_suitable_links(*, link: str = BASE_LINK, suitable_websites: list = SUITA
 
             if from_site in suitable_websites:
                 links_and_sources.append((url, from_site))
+        
+        return links_and_sources
+
+def scrape_recipe_based_on_source(*, url_and_source: tuple):
+    url, source = url_and_source
+    
+    match source:
+        case "Allrecipes":
+            return AllRecipes(url=url)
+        case "The Spruce Eats":
+            return SpruceEats(url=url)
+        case "EatingWell":
+            return EatingWell(url=url)
+        case "Simply Recipes":
+            return SimplyRecipies(url=url)
+        case "Serious Eats":
+            return SeriousEats(url=url)
             
